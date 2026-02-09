cfd680dd55a6f917998a3dc4ab6313e7875245ef
AJustcaTA <hihigood123321@gmail.com>
2026-02-10 02:53:28 +0800

    refactor: remove rev/ooo and rev/the-fish challenges

--- tools/combine_writeups.py
+++ tools/combine_writeups.py
@@ -16,6 +16,8 @@ def get_writeups(base_path):
         for chall in challenges:
             if cat == "crypto" and chall in ["six seven", "misdirection", "smol cats"]:
                 continue
+            if cat == "rev" and chall in ["ooo", "the-fish"]:
+                continue
 
             chall_path = os.path.join(cat_path, chall)
 
